51a0e3a7ddf700efff9084fec4b4a7ec6fe03a1d
Fabriyucra <fyucramita@alumno.unlam.edu.ar>
2024-06-11 23:50:12 -0300

    primer commit

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tiendaTallerWeb2' title`, () => {
+  it(`should have as title 'TallerWeb2Tienda'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tiendaTallerWeb2');
+    expect(app.title).toEqual('TallerWeb2Tienda');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tiendaTallerWeb2');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('TallerWeb2Tienda app is running!');
   });
 });
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tiendaTallerWeb2';
+  title = 'TallerWeb2Tienda';
 }
